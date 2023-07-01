1592110a07916b57193df04b32dc6cb7c46b9cbf
y6umbiu <138317735+y6umbiu@users.noreply.github.com>
2023-07-02 00:55:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
 
 $variants = [
-    "Fortnite"=>2,
-    "Valorant (Fun)"=>1,
-    "M&B 2"=>1,
-    "Football Manager 2024"=>1
+    "Valorant (Fun)",
+    "M&B 2",
+    "Football Manager 2024",
+    "Fortnite"
 ];
 
+$numbers = range(1, 10); 
 
-$randomOutcome = array_rand($variants, 1);
-echo "Выбранный исход: $randomOutcome";
+$mergedArray = [
+    $variants[0] => [$numbers[0], $numbers[1]],
+    $variants[1] => [$numbers[2], $numbers[3]],
+    $variants[2] => [$numbers[4], $numbers[5]],
+    $variants[3] => array_slice($numbers, 6)
+];
+
+$randomNumber = $numbers[array_rand($numbers)]; 
+$randomVariant = null; 
+
+foreach ($mergedArray as $variant => $range) {
+    if (in_array($randomNumber, $range)) {
+        $randomVariant = $variant;
+        break;
+    }
+}
+
+echo "Наконецто нормальная игра: $randomVariant";
+
 ?>
+
